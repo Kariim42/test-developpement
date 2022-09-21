b9edb3dad9cc264bf870982495092fee91dd5127
Kariim42 <minouche42152@gmail.com>
2022-09-21 12:07:07 +0200

    create header component and first part of homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login/login.component';
+import { LayoutComponent } from './layout/layout.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LoginComponent,
+    LayoutComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
